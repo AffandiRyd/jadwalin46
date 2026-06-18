4c1a78a032e89b56af9b1375eb4e198fd68d08ac
Zyxel <fahriaffandi.ryd@gmail.com>
2026-06-18 08:45:17 +0700

    deploy7

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
-import { app } from '../server';
+// /api/index.ts
+// Mengimpor file hasil build (server.cjs) yang sudah menyertakan semua depedency
+const app = require('../dist/server.cjs');
 
-export default app;
+module.exports = app;
