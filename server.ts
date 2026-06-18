fc81d7ad842a85d593d42dceabfead43d237f625
Zyxel <fahriaffandi.ryd@gmail.com>
2026-06-18 08:13:32 +0700

    deploy5

--- server.ts
+++ server.ts
@@ -287,7 +287,7 @@ async function checkAndSeedSupabase() {
 export const app = express();
 
 async function startServer() {
-  const PORT = process.env.PORT || 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
